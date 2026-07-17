9005d863c04a5a2ee3bd370aaf5bc9393d7633d7
Bazelais Fanfan <Bazelaisfanfan10@gmail.com>
2026-07-17 14:46:43 -0400

    fix firebase auth admin initialization

--- src/lib/firebaseAuthAdmin.ts
+++ src/lib/firebaseAuthAdmin.ts
@@ -4,18 +4,57 @@ import {
 
 
 import {
-  getApps
+  getApps,
+  initializeApp,
+  cert
 } from "firebase-admin/app";
 
 
-if(!getApps().length){
-  throw new Error(
-    "Firebase Admin non initialisé"
-  );
-}
+
+const firebaseAdminConfig = {
+
+  credential: cert({
+
+    projectId:
+      process.env.FIREBASE_PROJECT_ID,
+
+    clientEmail:
+      process.env.FIREBASE_CLIENT_EMAIL,
+
+    privateKey:
+      process.env.FIREBASE_PRIVATE_KEY
+      ?.replace(/\\n/g,"\n"),
+
+  }),
+
+
+  databaseURL:
+    process.env.FIREBASE_DATABASE_URL ||
+    "https://domino-fad16-default-rtdb.firebaseio.com"
+
+};
+
+
+
+
+
+const app =
+
+getApps().length
+
+?
+
+getApps()[0]
+
+:
+
+initializeApp(
+  firebaseAdminConfig
+);
+
+
+
 
 
 export const adminAuth =
-getAuth(
-  getApps()[0]
-);
+getAuth(app);
