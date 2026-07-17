d7382e560ea8b141dd71e6ac96a70a14461f07e9
Bazelais Fanfan <Bazelaisfanfan10@gmail.com>
2026-07-17 14:42:50 -0400

    fix all firebase admin auth imports

--- src/lib/firebaseAuthAdmin.ts
+++ src/lib/firebaseAuthAdmin.ts
@@ -2,9 +2,17 @@ import {
   getAuth
 } from "firebase-admin/auth";
 
+
 import {
   getApps
 } from "firebase-admin/app";
+
+
+if(!getApps().length){
+  throw new Error(
+    "Firebase Admin non initialisé"
+  );
+}
 
 
 export const adminAuth =
